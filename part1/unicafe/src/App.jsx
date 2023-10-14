9d0abd2d80a694889f7bcc34192fbf512d689a35
Niroasd <123634396+Niroasd@users.noreply.github.com>
2023-10-14 11:56:05 +0300

    Version 1.7 done.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,20 +7,25 @@ const Button = ({handleClick, text}) => {
 }
 
 const Display = ({text, data}) => <div>{text} {data}</div>
+const DisplayPercentage = ({text, data}) => <div>{text} {data} %</div>
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleGoodClick = () =>{
     setGood(good + 1)
+    setTotal(total + 1)
   }
   const handleNeutralClick = () =>{
     setNeutral(neutral + 1)
+    setTotal(total + 1)
   }
   const handleBadClick = () =>{
     setBad(bad + 1)
+    setTotal(total + 1)
   }
 
   return(
@@ -30,9 +35,12 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text={'neutral'}/>
       <Button handleClick={handleBadClick} text={'bad'}/>
       <h1>statistics</h1>
-      <Display text={'good'} data={good} />
-      <Display text={'neutral'} data={neutral} />
-      <Display text={'bad'} data={bad} />
+      <Display text={'good:'} data={good} />
+      <Display text={'neutral:'} data={neutral} />
+      <Display text={'bad:'} data={bad} />
+      <Display text={'total:'} data={total} />
+      <Display text={'average:'} data={((good - bad) / total)} />
+      <DisplayPercentage text={'positive:'} data={(good/total) * 100} />
     </div>
   )
 }
