a5bf2e52d9423a8b9f8ec1f5889f43c4042cf9b3
Niroasd <123634396+Niroasd@users.noreply.github.com>
2023-10-19 10:04:42 +0300

    Ver 2.8 done.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const Person = ({persons}) => {
-  const personList = persons.map(person => <p key={person.name}>{person.name}</p>)
+  const personList = persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
   return(
     <div>
       {personList}
@@ -11,29 +11,39 @@ const Person = ({persons}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleInputChange = (event) =>{
+  const handleNameInputChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberInputChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
 
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     const nameList = persons.map(person => {
       return person.name
     })
 
     event.preventDefault()
     const personObject = {
-      name : newName
+      name : newName,
+      number : newNumber
     }
 
     if(!nameList.includes(newName)){
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     } 
     else {
       alert(`${newName} is already added to phonebook.`)
@@ -45,15 +55,20 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={handleInputChange}/>
+          name: <input onChange={handleNameInputChange} value={newName}/>
         </div>
         <div>
-          <button onClick={addName} type="submit">add</button>
+          number: <input onChange={handleNumberInputChange} value={newNumber}/>
+        </div>
+        <div>
+          <button onClick={addPerson} type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <Person persons={persons}/>
-      <div>debug: {newName}</div>
+      <div>debug name: {newName}</div>
+      <div>debug number: {newNumber}</div>
     </div>
   )
 }
