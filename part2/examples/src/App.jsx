4734237d8083bd3e0ba045937fde9b20f50996f0
Niroasd <nico.roininen@edu.turkuamk.fi>
2024-09-09 10:41:46 +0300

    Added better error message to example

--- part2/examples/src/App.jsx
+++ part2/examples/src/App.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -40,9 +42,13 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note ${note.content} was already removed from the server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000);
+
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -58,6 +64,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
