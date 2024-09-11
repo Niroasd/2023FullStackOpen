717c64f2cbbcdeac2679b14744a9d399cde43817
Niroasd <123634396+Niroasd@users.noreply.github.com>
2024-09-11 13:55:04 +0300

    Exercise 2.18 done.

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,15 +1,51 @@
-const Country = ({countries, searchText}) => {
+const Country = ({ countries, searchText }) => {
     const filteredList = countries.filter(country => country.name.common.toLowerCase().includes(searchText))
     const filteredCountryList = filteredList.map(country =>
-      <li key={country.name.common}>
-        {country.name.common}
-      </li>)
-  
-    return(
-      <div>
-        {filteredCountryList}
-      </div>
-    )
+        <li key={country.name.common}>
+            {country.name.common}
+        </li>)
+        
+    const filteredCountrySingle = filteredList.map(country =>
+        <li key={country.name.common}>
+            <h1> {country.name.common} </h1>
+            <div>Capital {country.capital}</div>
+            <div>Area {country.area}</div>
+            <h3>languages:</h3>
+            <ul>
+                {/* {console.log(`${country.languages}`)} */}
+                {filteredCountryList.length == 1 ?
+                    Object.values(country.languages).map(lang =>
+                        <li key={lang}>{lang}</li>
+                    )
+                    : null
+                }
+            </ul>
+            <img src={country.flags.png}></img>
+        </li>)
+
+
+    if (filteredCountryList.length == 1) {
+        return (
+            <div>
+                {filteredCountrySingle}
+            </div>
+        )
+    }
+
+    if (filteredCountryList.length <= 10) {
+        return (
+            <div>
+                {filteredCountryList}
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                Too many results
+            </div>
+        )
+    }
+
 }
 
 export default Country
