88b632c501b18e6d25f180fde656ee3f67dc37c1
Niroasd <123634396+Niroasd@users.noreply.github.com>
2024-09-15 17:33:40 +0300

    3.3 Done.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,29 +6,39 @@ app.use(express.json())
 
 let data = [
     {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
+        id: "1",
+        name: "Arto Hellas",
+        number: "040-123456"
     },
     {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
+        id: "2",
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
     },
     {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
+        id: "3",
+        name: "Dan Abramov",
+        number: "12-43-234345"
     },
     {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
+        id: "4",
+        name: "Mary Poppendieck",
+        number: "39-23-6423122"
     }
 ]
 
 app.get('/api/persons', (request, response) => {
     response.json(data)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = data.find(p => p.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.get('/info', (request, response) => {
