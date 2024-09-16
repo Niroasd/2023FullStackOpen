12b98f69e62116cb73ddd5c35e052f936f91da61
Niroasd <123634396+Niroasd@users.noreply.github.com>
2024-09-17 00:57:19 +0300

    3.5 Done.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -27,6 +27,10 @@ let data = [
     }
 ]
 
+const generateId = () => {
+    return Math.floor(Math.random()*10000)
+  }
+
 app.get('/api/persons', (request, response) => {
     response.json(data)
 })
@@ -52,6 +56,29 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     data = data.filter(p => p.id !== id)
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+
+    const body = request.body
+
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+  
+    data = data.concat(person)
+
+    console.log(person)
+    response.json(person)
+
 })
 
 const PORT = 3001
