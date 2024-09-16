6cee45c5264506a6bce4c9f243dcc14b2af319cf
Niroasd <123634396+Niroasd@users.noreply.github.com>
2024-09-17 01:06:24 +0300

    Exercise 3.6 done.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -64,8 +64,14 @@ app.post('/api/persons', (request, response) => {
 
     if (!body.name || !body.number) {
       return response.status(400).json({ 
-        error: 'content missing' 
+        error: 'Name or number is missing' 
       })
+    }
+
+    if(data.find(p => p.name === body.name)){
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
     }
   
     const person = {
