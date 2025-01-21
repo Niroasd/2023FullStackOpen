08249e35f229b73ba1823d56603f23362de0c25c
Niroasd <123634396+Niroasd@users.noreply.github.com>
2025-01-21 20:36:47 +0200

    3.17 done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -56,6 +56,19 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id).then(result => {
         response.status(204).end()
