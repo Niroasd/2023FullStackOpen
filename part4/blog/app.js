23253c0e757a8a880f4c03ca2074fbaa3d5ef5d4
Niroasd <123634396+Niroasd@users.noreply.github.com>
2025-01-28 11:38:34 +0200

    turned logging to module

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 
 mongoose.set('strictQuery', false)
 app.use(cors())
@@ -13,7 +14,7 @@ app.use(express.json())
 app.use('/api/blogs/', blogsRouter)
 
 mongoose.connect(config.MONGODB_URI).then(() => {
-    console.log('connected to MongoDB');
-}).catch((error) => { console.log(error) });
+    logger.info('connected to MongoDB');
+}).catch((error) => { logger.error(error) });
 
 module.exports = { app }
