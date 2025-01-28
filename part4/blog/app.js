6fea7e9530b79cc08150b2c3798e1b3b09653fb5
Niroasd <123634396+Niroasd@users.noreply.github.com>
2025-01-28 11:31:08 +0200

    fixed body parsing

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,9 +5,12 @@ const cors = require('cors')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 
-app.use('/api/blogs/', blogsRouter)
+mongoose.set('strictQuery', false)
 app.use(cors())
 app.use(express.json())
+
+
+app.use('/api/blogs/', blogsRouter)
 
 mongoose.connect(config.MONGODB_URI).then(() => {
     console.log('connected to MongoDB');
