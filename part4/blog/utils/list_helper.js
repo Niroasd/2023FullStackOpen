5e0475d9bd56e5db02f4130e63a57ef6178ec9b9
Niroasd <123634396+Niroasd@users.noreply.github.com>
2025-01-29 02:11:00 +0200

    4.4 Done

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((likes, newLikes) => likes + newLikes.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
