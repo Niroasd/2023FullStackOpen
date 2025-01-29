1f1129c3181321ddc945cebfff7af8668446bb60
Niroasd <123634396+Niroasd@users.noreply.github.com>
2025-01-29 03:11:24 +0200

    4.5 Done

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,6 +6,11 @@ const totalLikes = (blogs) => {
   return blogs.reduce((likes, newLikes) => likes + newLikes.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) { return 'no blogs found' }
+  return blogs.reduce((best, { likes, author, title }) => best.likes > likes ? best : { title, author, likes }, 0)
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
