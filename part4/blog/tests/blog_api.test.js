1b57097adf3fe9dc1c45b1c7aed6ba26570fa489
Niroasd <123634396+Niroasd@users.noreply.github.com>
2025-02-16 13:56:05 +0200

    4.8 done

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,38 @@
+const { test, describe, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const Blog = require('../models/blog')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    "title": "no coping here",
+    "author": "asdf",
+    "url": "testurl.com",
+    "likes": 2
+  },
+  {
+    "title": "4.8 is done soon",
+    "author": "jane doe maybe who knows",
+    "url": "testurl.com",
+    "likes": 6
+  }
+]
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blogList = initialBlogs.map(blog => new Blog(blog))
+  const promiseArr = (blogList.map(blog => blog.save()))
+  await Promise.all(promiseArr)
+})
+
+describe('4.8 tests under this', () => {
+test('api returns correct amount of blogs', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, 2)
+})
+test('blogs return in json format', async () => {
+  await api.get('/api/blogs').expect('Content-Type', /application\/json/)
+})
+})
